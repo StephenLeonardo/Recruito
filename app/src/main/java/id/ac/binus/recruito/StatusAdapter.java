25a5085995d04771060295683a2e12984ce93a82
StephenLeonardo <stephenleonardo18@gmail.com>
2020-02-01 10:47:32 +0700

    Added AddPersonalInformationActivity

--- app/src/main/java/id/ac/binus/recruito/StatusAdapter.java
+++ app/src/main/java/id/ac/binus/recruito/StatusAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +12,7 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class CategoryAdapter extends ArrayAdapter<CategoryItem> {
-    @NonNull
-    @Override
+public class StatusAdapter extends ArrayAdapter<StatusItem>{
     public View getView(int Position, @Nullable View ConvertView, @NonNull ViewGroup Parent) {
         return initView(Position, ConvertView, Parent);
     }
@@ -25,27 +22,26 @@ public class CategoryAdapter extends ArrayAdapter<CategoryItem> {
         return initView(Position, ConvertView, Parent);
     }
 
-    public CategoryAdapter(@NonNull Context context, ArrayList<CategoryItem> CategoryList) {
-        super(context, 0, CategoryList);
+    public StatusAdapter(@NonNull Context context, ArrayList<StatusItem> StatusList) {
+        super(context, 0, StatusList);
     }
 
     private View initView(int Position, View ConvertView, ViewGroup Parent){
         if(ConvertView == null){
             ConvertView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.category_spinner_row, Parent, false
+                    R.layout.spinner_status_row, Parent, false
             );
         }
 
-        TextView CategoryName = ConvertView.findViewById(R.id.text_view_category);
+        TextView CategoryName = ConvertView.findViewById(R.id.text_view_status);
 
-        CategoryItem CurrentItem = getItem(Position);
+        StatusItem CurrentItem = getItem(Position);
 
         if(CurrentItem != null) {
-            CategoryName.setText(CurrentItem.getCategoryName());
+            CategoryName.setText(CurrentItem.getStatusName());
         }
 
         return ConvertView;
 
     }
-
 }
